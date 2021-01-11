3366716b79dd63546095faae07902f94006eeee2
yokokida <a.pirlo1109@gmail.com>
2021-01-11 21:38:39 +0900

    模倣

--- lib/pages/page_talk.dart
+++ lib/pages/page_talk.dart
@@ -1,3 +1,4 @@
+import 'package:demo/parts/tile.dart';
 import 'package:flutter/material.dart';
 
 class Talk extends StatelessWidget {
@@ -7,29 +8,43 @@ class Talk extends StatelessWidget {
       appBar: AppBar(
         title: Text("トーク"),
       ),
-      // ==== リストビューを追加 ==========
+      // ===== 編集 =====================
       body: ListView(
-          padding: const EdgeInsets.all(8),
+        // padding: const EdgeInsets.all(8),
           children: <Widget>[
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-            ListTile(title: Text("無駄")),
-          ]
-      ),
-      // ===============================
+            Tile(
+              icon: Icons.person,
+              username: "鹿太郎",
+              message: "しかし、鹿しかいない",
+            ),
+            Tile(
+              icon: Icons.person,
+              username: "久米酒",
+              message: "おいしいよー",
+            ),
+            Tile(icon: Icons.person, username: "くら", message: "とっても美味しい沖縄のお酒"),
+            Tile(icon: Icons.person, username: "団長", message: "止まるんじゃ、ねぇぞ"),
+            Tile(icon: Icons.person, username: "サルーイン", message: "こい"),
+            Tile(
+                icon: Icons.person,
+                username: "がらはど",
+                message: "だめだ！いくら積まれても..."),
+            Tile(
+                icon: Icons.person,
+                username: "太郎",
+                message: "だめだ、久しぶりにキレちまったよ"),
+            Tile(
+                icon: Icons.person,
+                username: "Harry",
+                message: "エクスペクト・パトローナーーム"),
+            Tile(
+                icon: Icons.person,
+                username: "くろひげ",
+                message: "似合ってるぜぃ、そのきずぅ〜"),
+            Tile(icon: Icons.person, username: "あすらん", message: "キラァァァァ"),
+            Tile(icon: Icons.person, username: "知人B", message: "1.14 release !"),
+          ]),
+      // ================================
     );
   }
 }
---
+++ lib/parts/tile.dart
@@ -0,0 +1,77 @@
+import 'package:demo/pages/screen_chat.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+
+class Tile extends StatelessWidget {
+  // == 追加 ========
+  IconData icon;
+  String username;
+  String message;
+  // ===============
+
+  // == 追加 ========
+  Tile({IconData icon, String username, String message}) {
+    this.icon = icon;
+    this.username = username;
+    this.message = message;
+  }
+  // ===============
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.20,
+      child: Container(
+        color: Colors.white,
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Icon(this.icon), // <- 追加：アイコンの設定
+            backgroundColor: Colors.pink,
+          ),
+          title: Text(this.username), // <- 追加：ユーザ名の設定
+          subtitle: Text(this.message), // <- 追加：メッセージの設定
+          onTap: () => {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Chat(
+                      username: this.username,
+                )))
+          },
+        ),
+      ),
+      actions: <Widget>[
+        IconSlideAction(
+          color: Colors.blue,
+          icon: Icons.flash_off,
+          onTap: () => {}, // _showSnackBar('Archive'),
+        ),
+        IconSlideAction(
+          color: Colors.indigo,
+          icon: Icons.volume_off,
+          onTap: () => {}, // _showSnackBar('Share'),
+        ),
+      ],
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          color: Colors.black45,
+          iconWidget: Text(
+            "非表示",
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () => {}, // _showSnackBar('More'),
+        ),
+        IconSlideAction(
+          color: Colors.red,
+          iconWidget: Text(
+            "削除",
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () => {}, // _showSnackBar('Delete'),
+        ),
+      ],
+    );
+  }
+}
