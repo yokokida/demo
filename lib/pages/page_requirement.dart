d355cd5c5253013649d9e0cf81d21bdf53b87bd8
yokokida <a.pirlo1109@gmail.com>
2021-01-14 22:57:37 +0900

    募集要項

--- lib/pages/page_requirement.dart
+++ lib/pages/page_requirement.dart
@@ -8,45 +8,37 @@ class Requirement extends StatelessWidget {
       appBar: AppBar(
         title: Text("ホーム"), // <- (※2)
       ),
-      body: Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.arrow_drop_down_circle),
-              title: const Text('Card title 1'),
-              subtitle: Text(
-                'Secondary Text',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 100.0,
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: [
+              Container(
+                child: Image.network('https://shop.ng-life.jp/wp-content/themes/chokuso/scripts/timthumb.php?w=984&q=90&zc=1&src=/wp-content/uploads/ichikawa/ichikawa_mainimege.jpg'),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
+              ListTile(
+                leading: Icon(Icons.arrow_drop_down_circle),
+                title: const Text('市川屋'),
+                subtitle: Text(
+                  'おもち（杵つきのし餅）',
+                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                ),
+                trailing: Icon(Icons.favorite_border),
               ),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.start,
-              children: [
-                FlatButton(
-                  textColor: const Color(0xFF6200EE),
-                  onPressed: () {
-                    // Perform some action
-                  },
-                  child: const Text('ACTION 1'),
+              Container(
+                height: 120.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    '江戸の時代から160年以上続くお餅屋さんが、厳選した県内産もち米を使って、一臼一臼丹念につきあげた、珠玉の杵つき餅！',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
                 ),
-                FlatButton(
-                  textColor: const Color(0xFF6200EE),
-                  onPressed: () {
-                    // Perform some action
-                  },
-                  child: const Text('ACTION 2'),
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
